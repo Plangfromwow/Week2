76f26a66b2aa8be1197c54a9690e9f8b062ca998
Jacob Garcia <jacobgarcia@rocketmortgage.com>
2022-08-08 14:55:29 -0400

    added some more files and the beginning of Student Database excersise.

--- ArrayDemo2/Program.cs
+++ ArrayDemo2/Program.cs
@@ -8,47 +8,57 @@ for (int i = 0; i < names.Length; i++)
     Console.WriteLine($"{names[i]} has grade {grades[i]}");
 }
 
-// ask the user which one to print out. 
-//Console.WriteLine();
-//Console.WriteLine("Please enter a number:");
-//string entry = Console.ReadLine();
-//int num = int.Parse(entry);
-//Console.WriteLine($"{names[num]} has grades {grades[num]}");
-
-//ask user which one to print out starting at 1 
-// so if they type in 1 we want to give them back index 0. We have to minus 1 OMEGALUL
-//version 1
-//Console.WriteLine();
-//Console.WriteLine("Please enter a number from 1-5:");
-//string entry = Console.ReadLine();
-//int num = int.Parse(entry);
-//num--;
-//Console.WriteLine($"{names[num]} has grades {grades[num]}");
-
-
-//version 2
-bool okay = false;
-int num = 0;
-while (!okay)
+do
 {
-    Console.WriteLine();
-    Console.WriteLine("Please enter a number from 1-5:");
-    string entry = Console.ReadLine();
-    bool validNumber = int.TryParse(entry, out num);
+	//version 2
+	bool okay = false;
+	int num = 0;
+	while (!okay)
+	{
+		Console.WriteLine();
+		Console.WriteLine("Please enter a number from 1-5:");
+		string entry = Console.ReadLine();
+		bool validNumber = int.TryParse(entry, out num);
 
-    if (validNumber)
-    {
-        if (num >= 1 && num <= 5)
-        {
-            okay = true;
-        }
-    }
-}
+		if (validNumber)
+		{
+			if (num >= 1 && num <= 5)
+			{
+				num--;
+				okay = true;
+			}
+		}
+	}
 
-// this code will run after they run a valid number.
-if (okay)
+	// this code will run after they run a valid number.
+
+
+	Console.WriteLine($"{names[num]} has grades {grades[num]}");
+} while (KeepGoing());
+
+
+static bool KeepGoing()
 {
-    num--;
-    Console.WriteLine($"{names[num]} has grades {grades[num]}");
+	while (true)
+	{
+		// Print out message asking if user wants to continue
+		Console.WriteLine("Would you like to go again? (y/n)");
+		// Ask the user for their input
+		string response = Console.ReadLine();
+		response = response.ToLower();
+		// Check if they typed "y". If so, return true.
+		// Otherwise return false.   YES   yes
+		if (response == "y" || response == "yes")
+		{
+			return true;
+		}
+		else if (response == "n" || response == "no")
+		{
+			return false;
+		}
+		else
+		{
+			Console.WriteLine("Please enter y or n");
+		}
+	}
 }
-
