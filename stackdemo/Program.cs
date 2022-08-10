349cc18a0a54fbdf779b0f80ee46b7ce7be64fdb
Jacob Garcia <jacobgarcia@rocketmortgage.com>
2022-08-10 13:39:37 -0400

    after lecture push

--- stackdemo/Program.cs
+++ stackdemo/Program.cs
@@ -1,4 +1,7 @@
-﻿
+﻿// this all demonstrates 2 data structures, stacks and queues 
+// different stack from the concept of stacks and heaps, this has nothing to do with those
+
+
 Stack<string> cards = new Stack<string>();
 
 cards.Push("3D");
@@ -41,4 +44,66 @@ Console.WriteLine(nums.Dequeue());
 foreach (int i in nums)
 {
     Console.WriteLine(i);
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Advanced stack stuff... oh no ");
+
+cards.Clear(); // empties out the stack 
+
+cards.Push("5D");
+cards.Push("JH");
+cards.Push("3S"); // 5 OF DIAMOND ON THE BOTTOM, JH NEXT, AND THEN 3S AT THE TOP
+
+Console.WriteLine(cards.Pop()); //expecting 3 of spades 
+
+string top = cards.Peek();
+int cardsLeft = cards.Count;
+Console.WriteLine($"The top is {top} but we didn't remove it. There are {cardsLeft} left.");
+
+Console.WriteLine(cards.Pop());
+cards.Push("9H");
+cards.Push("10D");
+cards.Push("KS");
+
+Console.WriteLine(cards.Pop());
+cards.Push("5D");
+
+Console.WriteLine("Drawing the rest of the cards:");
+while (cards.Count > 0)
+{
+    next = cards.Pop();
+    Console.WriteLine($"We just drew {next}");
+}
+
+Console.WriteLine($"The count of the deck is now {cards.Count}");
+
+// one more stack 
+cards.Clear();
+Console.WriteLine("We are putting 5 cards on and drawing them all off.");
+cards.Push("AD");
+cards.Push("2D");
+cards.Push("3D");
+cards.Push("4D");
+cards.Push("5D");
+
+while (cards.Count > 0)
+{
+    Console.WriteLine(cards.Pop());
+}
+
+// one more que demonstration 
+Console.WriteLine();
+Console.WriteLine("we are going to put 5 numbers in a que then go through the whole list.");
+nums.Clear();
+nums.Enqueue(1);
+nums.Enqueue(2);
+nums.Enqueue(3);
+nums.Enqueue(4);
+nums.Enqueue(5);
+
+while (nums.Count > 0)
+{
+    Console.WriteLine(nums.Dequeue());
 }
